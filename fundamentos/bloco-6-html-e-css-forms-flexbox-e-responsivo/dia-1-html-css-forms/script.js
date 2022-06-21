cf20bb3c74c2d7db1ec15f0ba5eb863bc85b9a5e
gabri10on <gabriel.r_00@hotmail.com>
2022-06-21 20:42:16 -0300

    pasta deletada

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,10 +1,13 @@
 const button = document.getElementById('buttonSubmit');
-const inputName = document.getElementById('input-name')
+const inputName = document.getElementById('input-name');
+const concordo = document.getElementById('concordo');
 
 
 function getValue(event) {
     event.preventDefault();
-    console.log('aaa');
+    console.log(concordo)
 }
 
 button.addEventListener('click', getValue)
+
+console.log(concordo)
